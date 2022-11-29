bf20c1c54a0011dbd41f808cda4254ac25855ae8
ElisaMadsen <e.leisa@hotmail.com>
2022-11-29 19:54:20 +0100

    Cost

--- src/main/java/com/example/rondobackend/repo/WholesaleCustomerRepo.java
+++ src/main/java/com/example/rondobackend/repo/WholesaleCustomerRepo.java
@@ -2,6 +2,7 @@ package com.example.rondobackend.repo;
 
 import com.example.rondobackend.model.WholesaleCostumer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface WholesaleCustomerRepo extends JpaRepository<WholesaleCostumer, Long> {
 }
